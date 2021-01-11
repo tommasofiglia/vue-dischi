34c92158b244b2e2f982d92af0bbf8e56fbf1dd3
Tommaso Figlia <tommasofiglia@gmail.com>
2021-01-11 21:53:24 +0100

    Esercizio completato

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
+//Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
+//Servendoci di Vue JS stampiamo tutto a schermo.
+
 let app = new Vue({
   // options object
   el: "#app",
+  data:{
+    cd: ""
+  },
+  methods:{
+
+  },
+  mounted(){
+    axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+    .then(random => {
+      this.cd = random.data.response;
+      console.log(this.cd);
+  });
+  }
+
 });
